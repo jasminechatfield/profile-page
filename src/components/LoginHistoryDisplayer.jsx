ae3183bbc7d16f0ccce002391d2f3445fa199216
Jasmine Chatfield <jazzchatfield@gmail.com>
2019-12-17 00:03:08 +0000

    Create LoginHistoryDisplayer

---
+++ src/components/LoginHistoryDisplayer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+class LoginHistoryDisplayer extends React.Component {
+  state = {
+    history: [
+      "Yesterday",
+      "The day before",
+      "The day before that",
+      "The day before the day before that",
+      "Etcetera"
+    ]
+  };
+  render() {
+    const { history } = this.state;
+    return (
+      <div className="LoginHistoryDisplayer">
+        <h2>Login History</h2>
+        <ul>
+          {history.map(date => {
+            return <li key="date">{date}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default LoginHistoryDisplayer;
